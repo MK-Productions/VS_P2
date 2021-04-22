058263436de90661e990f7b7d916418070395f99
Markus Kapp <mkapp1999@gmail.com>
2021-04-22 04:41:59 +0200

    first functional version with index.html

--- HTTPserv.c
+++ HTTPserv.c
@@ -16,62 +16,91 @@
 #include <sys/time.h>
 #include <sys/ioctl.h>
 #include <netdb.h>
+#include <sys/stat.h>
+
 
 #define SERVER_PORT 8080
 #define MAXLINE 4096
 #define SOCKET_ADDRESS struct sockaddr
+#define true 1
+#define false 0
 
 void printError(const char *errorMessage) {
-    printf("%s", errorMessage);
+    printf("%s \n", errorMessage);
+    exit(1);
+}
+
+// Quelle: https://stackoverflow.com/questions/8465006/how-do-i-concatenate-two-strings-in-c/8465083
+char* concat(const char *s1, const char *s2)
+{
+    const size_t len1 = strlen(s1);
+    const size_t len2 = strlen(s2);
+    char *result = malloc(len1 + len2 + 1);
+
+    memcpy(result, s1, len1);
+    memcpy(result + len1, s2, len2 + 1);
+
+    return result;
 }
 
 int main(int argc, char **argv) {
-    int sockfd;
-    int n;
-    int sendBytes;
+    int listenFD, connFD, n;
     struct sockaddr_in servaddr;
-    char sendLine[MAXLINE];
-    char receiveLine[MAXLINE];
+    uint8_t buff[MAXLINE + 1];
+    uint8_t receiveLine[MAXLINE + 1];
 
-    if (argc != 2) {
-        printError("server address error");
-    }
+    // read html file contents
+    int fd = open("docroot_2/folder/index.html", O_RDONLY);
+    struct stat sb;
+
+    fstat(fd, &sb);
+    char *contents = calloc(1, sb.st_size + 1);
+    read(fd, contents, sb.st_size);
+    close(fd);
 
     // create socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printError("Error while creating socket");
+    if ((listenFD = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printError("socket error!");
     }
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(SERVER_PORT);
 
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-        printError("inet_pton error");
+    if ((bind(listenFD, (SOCKET_ADDRESS *) &servaddr, sizeof(servaddr))) < 0) {
+        printError("bind error!");
     }
 
-    // connect to server
-    if (connect(sockfd, (SOCKET_ADDRESS *) &servaddr, sizeof(servaddr)) < 0) {
-        printError("connect failed!");
+    if ((listen(listenFD, 10)) < 0) {
+        printError("listen error!");
     }
 
-    sprintf(sendLine, "GET / HTTP/1.1\r\n\r\n");
-    sendBytes = strlen(sendLine);
+#pragma ide diagnostic ignored "EndlessLoop"
+    while (true) {
+        struct sockaddr_in address;
+        socklen_t addr_len;
 
-    if (write(sockfd, sendLine, sendBytes) != sendBytes) {
-        printError("write error");
-    }
-
-    // read server response
-    while ((n = read(sockfd, receiveLine, MAXLINE-1)) > 0) {
-        printf("%s", receiveLine);
-
+        connFD = accept(listenFD, (SOCKET_ADDRESS *) NULL, NULL);
         memset(receiveLine, 0, MAXLINE);
-    }
 
-    if (n < 0) {
-        printError("read error");
-    }
+        while ((n = read(connFD, receiveLine, MAXLINE - 1)) > 0) {
+            if (receiveLine[n - 1] == '\n') {
+                break;
+            }
 
-    exit(0);
+            memset(receiveLine, 0, MAXLINE);
+        }
+
+        if (n < 0) {
+            printError("read error!");
+        }
+
+        // send response
+        char* response = concat("HTTP/1.1 200 OK\r\n\r\n", contents);
+        snprintf((char *) buff, sizeof(buff), "%s", response);
+
+        write(connFD, (char *) buff, strlen((char *) buff));
+        close(connFD);
+    }
 }
