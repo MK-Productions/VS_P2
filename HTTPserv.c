595cf2888042fac19a0f5caad1f7b11c765d08eb
Markus Kapp <mkapp1999@gmail.com>
2021-04-24 16:24:42 +0200

    jpg not working

--- HTTPserv.c
+++ HTTPserv.c
@@ -18,29 +18,28 @@
 #include <netdb.h>
 #include <sys/stat.h>
 
-
-#define SERVER_PORT 8080
 #define MAXLINE 4096
 #define SOCKET_ADDRESS struct sockaddr
 #define true 1
 #define false 0
+
+int serverPort = 8080;
 
 void printError(const char *errorMessage) {
     printf("%s \n", errorMessage);
     exit(1);
 }
 
-// Quelle: https://stackoverflow.com/questions/8465006/how-do-i-concatenate-two-strings-in-c/8465083
-char* concat(const char *s1, const char *s2)
-{
-    const size_t len1 = strlen(s1);
-    const size_t len2 = strlen(s2);
-    char *result = malloc(len1 + len2 + 1);
+void exitHandler(int port) {
+    close(serverPort);
+    printf("\nClosed port!\n");
+    exit(0);
+}
 
-    memcpy(result, s1, len1);
-    memcpy(result + len1, s2, len2 + 1);
-
-    return result;
+const char *get_filename_ext(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if (!dot || dot == filename) return "";
+    return dot + 1;
 }
 
 int main(int argc, char **argv) {
@@ -48,25 +47,29 @@ int main(int argc, char **argv) {
     struct sockaddr_in servaddr;
     uint8_t buff[MAXLINE + 1];
     uint8_t receiveLine[MAXLINE + 1];
+    char docroot[MAXLINE];
+    char tstamp[128];            // Date/Time info
+    time_t td;                   // Current date&time
 
-    // read html file contents
-    int fd = open("docroot_2/folder/index.html", O_RDONLY);
-    struct stat sb;
+    if (argc >= 2) {
+        strcpy(docroot, argv[1]);
+    } else {
+        strcpy(docroot, "docroot_2/folder/"); // Use default directory
+    }
 
-    fstat(fd, &sb);
-    char *contents = calloc(1, sb.st_size + 1);
-    read(fd, contents, sb.st_size);
-    close(fd);
+    if (argc >= 3) {
+        serverPort = atoi(argv[2]);
+    }
 
     // create socket
     if ((listenFD = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printError("socket error!");
+        printError("socket error!\n");
     }
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(SERVER_PORT);
+    servaddr.sin_port = htons(serverPort);
 
     if ((bind(listenFD, (SOCKET_ADDRESS *) &servaddr, sizeof(servaddr))) < 0) {
         printError("bind error!");
@@ -76,31 +79,107 @@ int main(int argc, char **argv) {
         printError("listen error!");
     }
 
+    printf("Succesfully created TCP Socket listening %d %s", serverPort, "!\n");
+    fflush(stdout);
+
+    signal(SIGINT, exitHandler);
+
 #pragma ide diagnostic ignored "EndlessLoop"
     while (true) {
-        struct sockaddr_in address;
-        socklen_t addr_len;
-
         connFD = accept(listenFD, (SOCKET_ADDRESS *) NULL, NULL);
-        memset(receiveLine, 0, MAXLINE);
+        struct stat sb;
 
-        while ((n = read(connFD, receiveLine, MAXLINE - 1)) > 0) {
-            if (receiveLine[n - 1] == '\n') {
+        char serverReply[MAXLINE];
+        recv(connFD, serverReply, MAXLINE, 0);
+
+        if (strstr(serverReply, "favicon") != NULL) {
+            continue;
+        }
+
+        char path[100];
+        for (int i = 5; i < MAXLINE; i++) {
+            if (serverReply[i] == ' ') {
                 break;
             }
 
-            memset(receiveLine, 0, MAXLINE);
+            path[i - 5] = serverReply[i];
         }
 
-        if (n < 0) {
-            printError("read error!");
+        // read html file contents
+        char completePath[100];
+        strcat(completePath, docroot);
+        strcat(completePath, path);
+
+        memset(path, 0, 100);
+
+        char response[MAXLINE];
+        char *body;
+
+        if (access(completePath, F_OK) < 0) {
+            strcat(response, "HTTP/1.1 404 Not Found\n");
+            body = "404 not found";
+        } else {
+
+            // read file
+            FILE *fp = fopen(completePath, "rb");
+
+            stat(completePath, &sb);
+            // fstat(fd, &sb);
+
+            body = malloc(sb.st_size);
+
+            fread(body, sb.st_size, 1, fp);
+
+            fclose(fp);
+
+            time(&td);
+            strftime(tstamp, sizeof tstamp, "Date: %a, %d %b %G %T %Z\n", localtime(&td));
+
+            char lastModified[50];
+            strftime(lastModified, 50, "Last-modified: %a, %d %b %G %T %Z\n", localtime(&sb.st_mtime));
+
+            // status 200 OK Header
+            strcat(response, "HTTP/1.1 200 OK\n");
+            strcat(response, tstamp);
+            strcat(response, lastModified);
+            strcat(response, "Content-Language: de\n");
+
+            if (strcmp(get_filename_ext(completePath), "jpg") == 0) {
+                strcat(response, "Content-Type: image/jpeg\n");
+                strcat(response, "Content-Length: 20896\r\n\r\n");
+            } else {
+                strcat(response, "Content-Type: text/html; charset=iso8859-1\r\n\r\n");
+            }
         }
+
+        memset(completePath, 0, 100);
+
+//        strcat(response, body);
+
 
         // send response
-        char* response = concat("HTTP/1.1 200 OK\r\n\r\n", contents);
-        snprintf((char *) buff, sizeof(buff), "%s", response);
-
-        write(connFD, (char *) buff, strlen((char *) buff));
+//        write(connFD, response, sizeof(char) * strlen(response));
+        write(connFD, body, sb.st_size);
+        memset(response, 0, MAXLINE);
         close(connFD);
     }
 }
+
+/*
+ * TODO:
+ *      - docroot und port variable machen ✅
+ *      - aufgerufene Datei muss ausgelsen werden und in content gespeichert werden ✅
+ *          - datei auslesen muss in die schleife ✅
+ *          - falls Datei nicht gefunden wird 404 not found zurückgeben ✅
+ *          - Fehlerhandling beim Datei auslesen ✅
+ *      - Header vervollständigen ✅
+ *      - Bilder darstellen binär
+ *          - Content-Type binär
+ *      - printf, snprint durch write ersetzen
+ *      - nur verarbeitung GET Requests nachgucken
+ *      - mehrstufiger Server (fork i guess)
+ *      - Hinweise nochmal checken
+ *          - warum kompilieren mit -g
+ *          - valgrind
+ */
+
